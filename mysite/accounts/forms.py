d448bfa1623c3fdf5f695eb641f42b36cd6e6d4a
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-19 16:45:23 +0700

    add email to registr + edit password form + edit profile form

--- mysite/accounts/forms.py
+++ mysite/accounts/forms.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2', 'email']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,6 +16,7 @@ class RegisterForm(UserCreationForm):
         self.fields['username'].label = 'Имя пользователя'
         self.fields['password1'].label = 'Пароль'
         self.fields['password2'].label = 'Подтверждение пароля'
+        self.fields['email'].label = 'Адрес электронной почты'
 
         self.fields['username'].help_text = 'Не более 150 символов.<br /> Только буквы, цифры и @/./+/-/_.'
         self.fields['password1'].help_text = (
@@ -23,5 +24,25 @@ class RegisterForm(UserCreationForm):
             'Ваш пароль не может состоять только из цифр.'
         )
         self.fields['password2'].help_text = 'Повторите пароль для подтверждения.'
+        self.fields['email'].help_text = 'Введите ваш адрес электронной почты.'
+
+class PasswordChangeForm(forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput, label="Старый пароль")
+    new_password1 = forms.CharField(widget=forms.PasswordInput, label="Новый пароль")
+    new_password2 = forms.CharField(widget=forms.PasswordInput, label="Подтвердите новый пароль")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password1 = cleaned_data.get("new_password1")
+        new_password2 = cleaned_data.get("new_password2")
+
+        if new_password1 and new_password2 and new_password1 != new_password2:
+            raise forms.ValidationError("Новые пароли не совпадают.")
+        return cleaned_data
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
