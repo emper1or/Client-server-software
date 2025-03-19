9040acd5c8ec4d106acfbc7800cb9314947157fa
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-19 16:46:27 +0700

    add profiles html

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
