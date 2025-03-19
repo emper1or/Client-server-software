4fddab915c56c1c4464e3f7a957926ff0d95daff
Ivan Kusnetsov <emper1or38@yandex.ru>
2025-03-19 16:47:02 +0700

    add edit profile, profile and change password logic

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,10 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import redirect
-from django.shortcuts import render
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect
 from library.models import Book, BookCover
-
-from .forms import RegisterForm
+from .forms import UserEditForm, PasswordChangeForm
 
 
 def anonymous_required(user):
@@ -23,7 +24,7 @@ def register(request):
             return redirect('dashboard')
     else:
         form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form})
 
 
 @user_passes_test(anonymous_required, login_url='home')
@@ -36,8 +37,8 @@ def login_view(request):
             login(request, user)
             return redirect('dashboard')
         else:
-            return render(request, 'login.html', {'error': 'Неверное имя пользователя или пароль'})
-    return render(request, 'login.html')
+            return render(request, 'accounts/login.html', {'error': 'Неверное имя пользователя или пароль'})
+    return render(request, 'accounts/login.html')
 
 
 def logout_view(request):
@@ -56,8 +57,46 @@ def dashboard(request):
         'books': books,
         'covers': covers,
     }
-    return render(request, 'dashboard.html', context)
+    return render(request, 'accounts/dashboard.html', context)
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserEditForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Профиль успешно обновлен.')
+            return redirect('profile')
+    else:
+        form = UserEditForm(instance=request.user)
+    return render(request, 'accounts/edit_profile.html', {'form': form})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST)
+        if form.is_valid():
+            old_password = form.cleaned_data['old_password']
+            new_password = form.cleaned_data['new_password1']
+            if request.user.check_password(old_password):
+                request.user.set_password(new_password)
+                request.user.save()
+                update_session_auth_hash(request, request.user)  # Обновление сессии, чтобы пользователь не вышел
+                messages.success(request, 'Пароль успешно изменен.')
+                return redirect('profile')
+            else:
+                messages.error(request, 'Старый пароль введен неверно.')
+    else:
+        form = PasswordChangeForm()
+    return render(request, 'accounts/change_password.html', {'form': form})
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'accounts/home.html')
